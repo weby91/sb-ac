54e0fef21cdd70cc2c616099d53acb1c21b9c225
Webster <lebronwebz@gmail.com>
2024-10-08 00:24:32 +0700

    feat: add Navigation

--- app/src/main/java/jp/speakbuddy/edisonandroidexercise/MainActivity.kt
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/MainActivity.kt
@@ -7,21 +7,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import jp.speakbuddy.edisonandroidexercise.ui.fact.FactScreen
-import jp.speakbuddy.edisonandroidexercise.ui.fact.FactViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import jp.speakbuddy.edisonandroidexercise.presentation.navigation.Navigation
 import jp.speakbuddy.edisonandroidexercise.ui.theme.EdisonAndroidExerciseTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             EdisonAndroidExerciseTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    FactScreen(viewModel = FactViewModel())
+                    Navigation()
                 }
             }
         }
