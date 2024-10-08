3f2faa07981a36624f6a1171e7344985f0b37e2d
Webster <lebronwebz@gmail.com>
2024-10-08 17:14:23 +0700

    chore: bump versionName and versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "jp.speakbuddy.edisonandroidexercise"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
